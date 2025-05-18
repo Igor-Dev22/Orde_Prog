e42cad8eed60b44c9d3d403e2c883b058825fd97
Anderson Rodrigues <irdor.redna@gmail.com>
2025-05-18 15:46:52 -0300

    updated main

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
-}
+    int vet[10];
+    int i;
 
+    //Ler os valores recebidos
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &vet[i]);
+
+    }
+
+    //Impressão dos valores
+    for (i = 0; i < 10; i++) {
+        printf("%d\n", vet[i]);
+    }
+
+}
