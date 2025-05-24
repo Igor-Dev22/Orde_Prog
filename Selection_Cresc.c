c55c5095a5b0f97ac21f8aa4b1fc9cdd473440c8
Anderson Rodrigues <irdor.redna@gmail.com>
2025-05-24 01:49:17 -0300

    selection cresc created

--- Selection_Cresc.c
+++ Selection_Cresc.c
@@ -2,26 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Função de ordenação Selection Sort em ordem crescente
 void selectionSortCrescente(int vet[], int tamanho) {
     for (int i = 0; i < tamanho - 1; i++) {
         int min_idx = i;
 
-        // Encontra o índice do menor valor na parte não ordenada
         for (int j = i + 1; j < tamanho; j++) {
             if (vet[j] < vet[min_idx]) {
                 min_idx = j;
             }
         }
 
-        // Troca o menor valor encontrado com o da posição atual
         int temp = vet[i];
         vet[i] = vet[min_idx];
         vet[min_idx] = temp;
     }
 }
 
-// Função para imprimir os elementos do vetor
 void imprimirVetor(int vet[], int tamanho) {
     for (int i = 0; i < tamanho; i++) {
         printf("%d ", vet[i]);
@@ -29,7 +25,6 @@ void imprimirVetor(int vet[], int tamanho) {
     printf("\n");
 }
 
-// Função para preencher o vetor com valores aleatórios
 void vetor_aleatorio(int vet[], int tamanho) {
     for (int i = 0; i < tamanho; i++) {
         vet[i] = rand() % tamanho;
